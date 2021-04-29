62632cf485311e1dd3232aca3ef451cb1daefbec
DurayBence <duray.bence@users.noreply.github.com>
2021-04-29 10:38:35 +0200

    Create Army methods

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
@@ -1,3 +1,37 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Army {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+
+    List<MilitaryUnit> units = new ArrayList<>();
+
+    public void addUnit(MilitaryUnit militaryUnit) {
+        units.add(militaryUnit);
+    }
+
+    public void damageAll(int damage) {
+        for (int i = 0; i < units.size(); i++) {
+            MilitaryUnit currentUnit = units.get(i);
+            currentUnit.sufferDamage(damage);
+            if (currentUnit.getHitPoints() < 25) {
+                units.remove(currentUnit);
+                i--;
+            }
+        }
+    }
+
+    public int getArmyDamage() {
+        int result = 0;
+        for (MilitaryUnit mu : units) {
+            result += mu.doDamage();
+        }
+        return result;
+    }
+
+    public int getArmySize() {
+        return units.size();
+    }
+
+}
