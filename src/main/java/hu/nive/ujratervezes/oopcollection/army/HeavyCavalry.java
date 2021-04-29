058855662b9eb10560f173be0563f65d7dd98b62
DurayBence <duray.bence@users.noreply.github.com>
2021-04-29 10:31:39 +0200

    Implement specifics for HeavyCavalry and Swordsman

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
@@ -2,10 +2,22 @@ package hu.nive.ujratervezes.oopcollection.army;
 
 public final class HeavyCavalry extends MilitaryUnit {
 
+    boolean isCharging = true;
+
     public HeavyCavalry() {
         this.hitPoints = 150;
         this.damage = 20;
         this.isArmored = true;
     }
 
+    @Override
+    public int doDamage() {
+        int dealtDamage = damage;
+        if (isCharging) {
+            dealtDamage *= 3;
+            isCharging = false;
+        }
+
+        return dealtDamage;
+    }
 }
--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
@@ -2,10 +2,21 @@ package hu.nive.ujratervezes.oopcollection.army;
 
 public final class Swordsman extends MilitaryUnit {
 
+    boolean hasShield = true;
+
     public Swordsman(boolean isArmored) {
         this.hitPoints = 100;
         this.damage = 10;
         this.isArmored = isArmored;
     }
 
+    @Override
+    public void sufferDamage(int damage) {
+        if (hasShield) {
+            hasShield = false;
+            return; //no damage dealt
+        } else {
+            super.sufferDamage(damage);
+        }
+    }
 }
